5a8a3e09134ba73f347e4ffe60d867dc7e0d9c22
Jason Tame <jason@pango.studio>
2019-04-13 13:53:15 +0200

    Add resources route, model and controller

---
+++ app/Resource.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resource extends Model
+{
+    protected $fillable = ['filename', 'mime', 'size', 'title', 'description'];
+}
