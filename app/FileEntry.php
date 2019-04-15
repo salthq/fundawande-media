62a351bc09dbf84b2a5267c68fb42688ca3a2050
Jason Tame <jason@pango.studio>
2019-04-15 15:14:09 +0200

    Pluralize resources

--- app/FileEntry.php
+++ app/FileEntry.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class FileEntry extends Model
 {
-    protected $fillable = ['filename', 'mime', 'path', 'size'];
+    protected $fillable = ['filename', 'mime', 'size'];
 
 }
